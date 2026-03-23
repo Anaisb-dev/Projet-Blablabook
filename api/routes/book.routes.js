c975259dccd6dae66d96adf99d64cf9c71a3fb20
Chloe-Da-Silva <chloemescam40@gmail.com>
2026-03-23 21:55:07 +0100

    modification book.controlleur pour filtrer les données de l'api google book

--- api/routes/book.routes.js
+++ api/routes/book.routes.js
@@ -1,13 +1,21 @@
 // Router concernant les livres
 
 import express from "express";
+import { searchBooks } from "../controllers/book.controller.js";
 
 const router = express.Router();
-router.get('/');
+
+router.get('/', (req, res) => {
+    res.send("Liste des livres OK");
+});
 // Afficher les livres par genre et popularité
-router.get('/search');
+
+router.get('/search', searchBooks);
 // Rechercher un livre via Google Books
-router.get('/:id');
+
+router.get('/:id', (req, res) => {
+    res.send("Détail du livre OK");
+});
 // La page détail d'un livre via son id
 
 
