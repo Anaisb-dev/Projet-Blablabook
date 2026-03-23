ff42da6e435c17b23dbe45dd25e7179944aac692
Princillia-B <princillia.dev@gmail.com>
2026-03-23 12:05:45 +0100

    Renommage du menu de la version mobile en DropdwonMenu

--- client/src/lib/components/layout/Layout.svelte
+++ client/src/lib/components/layout/Layout.svelte
@@ -2,13 +2,13 @@
 <script>
     import LogoIcon from '../ui/LogoIcon.svelte';
     import BrandName from '../ui/BrandName.svelte';
-    import NavigationMenu from '../navigation/NavigationMenu.svelte';
+    import DropdownMenu from '../navigation/Dropdown Menu.svelte';
 </script>
 
 <!-- HTML -->
     <LogoIcon width={70} height={48} />
     <BrandName />
-    <NavigationMenu />
+    <DropdownMenu />
 
 <!-- CSS-->
 
