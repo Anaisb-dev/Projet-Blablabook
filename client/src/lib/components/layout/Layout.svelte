05fabfda4da05cadd4939787500adc673f76f1fa
Princillia-B <princillia.dev@gmail.com>
2026-03-23 10:54:30 +0100

    ajout du composant LogoIcon + test dans APP.Svelte

---
+++ client/src/lib/components/layout/Layout.svelte
@@ -0,0 +1,11 @@
+<!-- JS-->
+<script>
+    import LogoIcon from '../ui/LogoIcon.svelte';
+</script>
+
+<!-- HTML -->
+    <h1> test </h1>
+    <LogoIcon size={48} />
+
+<!-- CSS-->
+
---
+++ client/src/lib/components/ui/LogoIcon.svelte
@@ -0,0 +1,7 @@
+<!-- JS -->
+<script>
+    export let size = 24; // taille par défaut de l'icône (modifiable depuis l'extérieur du composant)
+</script>
+
+<!-- HTML -->
+<img src="/logo.png" alt="Logo BlablaBook" style="width: {size}px; height: {size}px;"/>
