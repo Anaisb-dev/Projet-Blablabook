c7022a79bbfa5221f8e9f6e286c92b6315d05f39
EmileCOURTOIS <emile.courtois@oclock.school>
2026-03-24 12:49:53 +0100

    ajout Footer a app.svelte

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,7 +1,10 @@
 <!-- JS -->
 <script>
   import Layout from "./lib/components/layout/Layout.svelte";
+  import Footer from "./lib/components/layout/Footer.svelte";
 </script>
 
 <!-- HTML -->
 <Layout />
+
+<Footer/>
