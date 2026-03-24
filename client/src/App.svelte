da6ec2cde846ee8d2e04af454c26e1f688cdc337
EmileCOURTOIS <emile.courtois@oclock.school>
2026-03-24 15:32:10 +0100

    barre de recherche

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -2,8 +2,13 @@
 <script>
   import Layout from "./lib/components/layout/Layout.svelte";
   import BookGrid from "./lib/components/LibrarySection/BookGrid.svelte";
+  import Searchbar from "./lib/components/layout/searchbar.svelte";
 </script>
 
 <!-- HTML -->
 <Layout />
-<BookGrid />
+
+<main class="p-4 max-w-3xl mx-auto">
+  <Searchbar />
+  <BookGrid />
+</main>
