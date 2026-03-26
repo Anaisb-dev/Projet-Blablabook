264a29f5f13e792364bffd97639171cbd540d3ca
Princillia-B <princillia.dev@gmail.com>
2026-03-26 19:51:09 +0100

    Barre de progression

--- client/src/lib/components/ui/Button.svelte
+++ client/src/lib/components/ui/Button.svelte
@@ -6,11 +6,20 @@
 		primary:"mt-10 w-[250px] rounded-xl border px-3 py-2 text-sm bg-[#BF9075] text-[#FFF7F1] flex items-center justify-center mx-auto",
 		icon: "mt-10 w-[40px] h-[40px] rounded-xl border flex items-center justify-center mx-auto hover:bg-[#F2E0D0] transition-colors duration-200",
 		iconText: "mt-3 w-[200px] h-[40px] rounded-xl border flex items-center justify-center hover:bg-[#F2E0D0] transition-colors duration-200",
-		myLibraryButton: "mt-3 w-[200px] h-[40px] rounded-xl border flex items-center justify-center bg-white hover:bg-[#F2E0D0] transition-colors duration-200"
+		myLibraryButton: "mt-3 w-[200px] h-[40px] rounded-xl border flex items-center justify-center hover:bg-[#F2E0D0] transition-colors duration-200"
 	};
 </script>
 
-<button class={`${styles[variant]} ${active ? "bg-[#F2E0D0]" : ""}`} on:click>
+<button
+	class={`${styles[variant] || styles.primary} ${
+		variant === "myLibraryButton"
+			? active
+				? "bg-[#BF9075] text-white cursor-default"
+				: "bg-white text-[#BF9075]"
+			: ""
+	}`}
+	on:click
+>
 	<slot></slot>
 </button>
 	<!-- styles.primary est un fallback au cas où une variante n'est pas définie, par défaut le bouton sera stylé avec la classe primary -->
