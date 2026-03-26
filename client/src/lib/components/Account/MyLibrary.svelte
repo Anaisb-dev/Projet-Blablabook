02aab66196b9f7f8b7052689836ab553b31d3a36
Princillia-B <princillia.dev@gmail.com>
2026-03-26 17:13:38 +0100

    Ajout des éléments de la page Mon Compte

--- client/src/lib/components/Account/MyLibrary.svelte
+++ client/src/lib/components/Account/MyLibrary.svelte
@@ -155,7 +155,7 @@
                 Annuler
             </button>
 
-            <!-- Barre progression -->
+            <!-- Barre de progression -->
 		<div class="w-full h-[4px] bg-gray-300 rounded overflow-hidden">
 			<div
 				class="h-full bg-[#F2E0D0] transition-all duration-100"
