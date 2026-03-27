e93fc5b769c15997d25c09072ce28211df265e56
Anaisb-dev <brnt.anais@gmail.com>
2026-03-27 12:08:10 +0100

    Connexion des inputs avec bind:value ok, testé avec un console.log ok

--- client/src/lib/components/RegisterForm/RegisterForm.svelte
+++ client/src/lib/components/RegisterForm/RegisterForm.svelte
@@ -3,20 +3,29 @@
     import PasswordInput from "../ui/PasswordInput.svelte";
     import Button from "../ui/Button.svelte";
     import Checkbox from "../ui/Checkbox.svelte";
+    import { registerUser } from "../../../../services/auth.service.js";
+
+    let username = "";
+    let email = "";
+    let password = "";
+    let confirmPassword = "";
+
+    $: console.log({ username, email, password, confirmPassword });
+
 </script>
 
 <h1 class="text-2xl font-bold mb-4 text-center p-10"> Créer un compte </h1>
 <p class="text-center">Pseudo</p>
-<Input placeholder="Entrez votre pseudo"/>
+<Input bind:value={username}/>
 
 <p class="text-center">Email</p>
-<Input placeholder="Entrez votre email"/>
+<Input bind:value={email}/>
 
 <p class="text-center">Mot de passe</p>
-<PasswordInput/>
+<PasswordInput bind:value={password}/>
 
 <p class="text-center">Confirmer le mot de passe</p>
-<PasswordInput/>
+<PasswordInput bind:value={confirmPassword}/>
 
 <Checkbox />
 <Button> Créer un compte </Button>
