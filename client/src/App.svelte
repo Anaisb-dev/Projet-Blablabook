d46a6d39bca8018491d23920b406f08b0f5e26cc
EmileCOURTOIS <emile.courtois@oclock.school>
2026-03-27 01:30:37 +0100

    page d'accueil presente manque juste du css et carousel fait

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,16 +1,17 @@
 <script>
   import Router from "svelte-spa-router";
   import Layout from "./lib/components/layout/Layout.svelte";
+  import HomePage from "./lib/components/HomePage/HomePage.svelte";
   import LibrarySection from "./lib/components/LibrarySection/LibrarySection.svelte";
   import RegisterForm from "./lib/components/RegisterForm/RegisterForm.svelte";
   import LoginForm from "./lib/components/LoginForm/LoginForm.svelte";
 
   const routes = {
-    "/": LibrarySection,
+    "/": HomePage,
     "/books": LibrarySection,
     "/register": RegisterForm,
     "/login": LoginForm,
-    "*": LibrarySection // fallback (si aucune route ne correspond on affiche cette page à changer par une page 404)
+    "*": HomePage // page 404a faire pour le moment on redirige vers l'accueil
   };
 </script>
 
