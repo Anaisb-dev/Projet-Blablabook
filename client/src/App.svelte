540ea4defaedfbc64f034b794c000db3019118f6
Chloe-Da-Silva <chloemescam40@gmail.com>
2026-03-27 10:19:36 +0100

    ok

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,18 +1,19 @@
 <script>
   import Router from "svelte-spa-router";
   import Layout from "./lib/components/layout/Layout.svelte";
+  import HomePage from "./lib/components/HomePage/HomePage.svelte";
   import LibrarySection from "./lib/components/LibrarySection/LibrarySection.svelte";
   import RegisterForm from "./lib/components/RegisterForm/RegisterForm.svelte";
   import LoginForm from "./lib/components/LoginForm/LoginForm.svelte";
   import Account from "./lib/components/Account/Account.svelte";
 
   const routes = {
-    "/": LibrarySection,
+    "/": HomePage,
     "/books": LibrarySection,
     "/register": RegisterForm,
     "/login": LoginForm,
     "/profile": Account,
-    "*": LibrarySection // fallback (si aucune route ne correspond on affiche cette page à changer par une page 404)
+    "*": HomePage // page 404 a faire pour le moment on redirige vers l'accueil
   };
 </script>
 
