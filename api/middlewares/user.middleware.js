7730449692f9bb67f3129a9d51fe9aecb4160a65
Princillia-B <princillia.dev@gmail.com>
2026-03-31 19:07:00 +0200

    ajout dans user mdiddleware, services, informationForm pour la modification du mot de passe

--- api/middlewares/user.middleware.js
+++ api/middlewares/user.middleware.js
@@ -54,7 +54,13 @@ export function validatePasswordUpdate(req, res, next) {
             .pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@*._-])[A-Za-z\d@*._-]+$/)
             // Doit contenir minuscule, majuscule, chiffre et caractère spécial
             .min(8)
-            .required(),
+            .required()
+            .messages({
+                'string.pattern.base': 'Le mot de passe doit contenir au moins 1 minuscule, 1 majuscule, 1 chiffre et 1 caractère spécial (@*._-)',
+                'string.min': 'Le mot de passe doit contenir au moins 8 caractères',
+                'string.empty': 'Le mot de passe est obligatoire',
+                'any.required': 'Le mot de passe est obligatoire'
+            }),
 
         confirmPassword: Joi.string()
             .valid(Joi.ref('password'))
