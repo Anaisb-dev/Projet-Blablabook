16546fc55f278af4b07009e7b291df569ac56df1
EmileCOURTOIS <emile.courtois@oclock.school>
2026-03-31 16:08:30 +0200

    feat: add FAQ page

--- client/src/lib/components/navigation/NavigationMenu.svelte
+++ client/src/lib/components/navigation/NavigationMenu.svelte
@@ -12,8 +12,6 @@
     <ul class="flex items-center gap-4">
         <li><a href="#/">Accueil</a></li>
         <li><a href="#/books">Bibliothèque</a></li>
-        <li><a href="#/faq">FAQ</a></li>
-
         {#if authStore.token}
             <li><a href="#/profile">Mon Compte</a></li>
             <li>
