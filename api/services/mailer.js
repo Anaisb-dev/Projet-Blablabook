defbab519444e2494dd951abe961557e5452ed81
Anaisb-dev <brnt.anais@gmail.com>
2026-06-04 19:09:09 +0200

    fix: changement de port nodemailer

--- api/services/mailer.js
+++ api/services/mailer.js
@@ -4,8 +4,8 @@ import nodemailer from "nodemailer";
 
 export const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
-    port: 587,
-    secure: false,
+    port: 465,
+    secure: true, // true pour le port 465
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
